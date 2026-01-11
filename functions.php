73f9a5e4037df4a825db80fdb57c0541fb9bcfc6
Seán Stickle <sean@stickle.net>
2026-01-11 14:01:59 -0500

    Bump version to 0.2.2 and add sigma_signet_get_openurl_resolver() function

--- functions.php
+++ functions.php
@@ -21,3 +21,21 @@ if (!function_exists('sigma_signet_get_user_type')) {
         return $sigma_id_type ?: 'wordpress';
     }
 }
+
+if (!function_exists('sigma_signet_get_openurl_resolver')) {
+    /**
+     * Get the current user's OpenURL resolver URL
+     *
+     * @return string|null The resolver URL or null if not set or user not logged in
+     */
+    function sigma_signet_get_openurl_resolver(): ?string
+    {
+        if (!is_user_logged_in()) {
+            return null;
+        }
+
+        $resolver = get_user_meta(get_current_user_id(), 'sigma_openurl_resolver', true);
+
+        return $resolver ?: null;
+    }
+}
